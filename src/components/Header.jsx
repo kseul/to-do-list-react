03c635799106ba75dd8c0e45fa643e880604ada4
seul <kseul06@gmail.com>
2023-02-07 17:16:53 +0900

    Add : Header components UI
    - Header 컴포넌트의 UI작성
    - 글로벌 폰트 적용

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,37 @@
 import styled from 'styled-components';
+import { RiMoonClearLine } from 'react-icons/ri';
 
 const Header = () => {
   return (
     <HeaderContainer>
-      <div>헤더</div>
+      <Title>My Task</Title>
+      <IconWrapper>
+        <RiMoonClearLine size={23} />
+      </IconWrapper>
     </HeaderContainer>
   );
 };
 
 const HeaderContainer = styled.header`
-  background-color: yellow;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 5rem;
+  padding: 0 2rem;
+  border-radius: 1.5rem 1.5rem 0 0;
+  background-color: #e5e7ea;
+`;
+
+const Title = styled.h2`
+  font-size: 1.2rem;
+  font-weight: 500;
+`;
+
+const IconWrapper = styled.div`
+  &:hover {
+    cursor: pointer;
+  }
 `;
 
 export default Header;
--- src/styles/GlobalStyles.jsx
+++ src/styles/GlobalStyles.jsx
@@ -3,12 +3,13 @@ import { createGlobalStyle } from 'styled-components';
 const GlobalStyle = createGlobalStyle`
     * {
         box-sizing: border-box;
+        color: #303030;
     }
 
     body{
         padding: 0;
         margin: 0;
-        font-family: 'Noto Sans KR', sans-serif;
+        font-family: 'Poppins','Noto Sans KR', sans-serif;
     };
 
     button{
