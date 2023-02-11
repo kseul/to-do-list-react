5eff2e094dc8b29acaaa0f9ad90c9112b71fcd26
seul <kseul06@gmail.com>
2023-02-11 16:38:13 +0900

    Solution : LocalStorage 저장
    +) input - label 버그수정

--- src/Solution/components/Todo/Todo.jsx
+++ src/Solution/components/Todo/Todo.jsx
@@ -3,7 +3,7 @@ import { FaTrashAlt } from 'react-icons/fa';
 import styles from './Todo.module.css';
 
 const Todo = ({ todo, onUpdate, onDelete }) => {
-  const { text, status } = todo;
+  const { id, text, status } = todo;
 
   // 📌 삼항연산자로 두가지 상태를 컨트롤하여 상위 onUpdate컴포넌트에 올려준다. 즉, 이곳에서 변경된 체크상태를 이곳 컴포넌트에서 값을 조절 -> 상위로 업데이트되도록 올려줌
   const handleChange = (e) => {
@@ -18,11 +18,11 @@ const Todo = ({ todo, onUpdate, onDelete }) => {
       <input
         className={styles.checkbox}
         type='checkbox'
-        id='checkbox'
+        id={`checkbox${id}`}
         checked={status === 'completed'}
         onChange={handleChange}
       />
-      <label className={styles.text} htmlFor='checkbox'>
+      <label className={styles.text} htmlFor={`checkbox${id}`}>
         {text}
       </label>
       <span className={styles.icon}>
--- src/Solution/components/TodoList/TodoList.jsx
+++ src/Solution/components/TodoList/TodoList.jsx
@@ -1,10 +1,16 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import AddTodo from '../AddTodo/AddTodo';
 import Todo from '../Todo/Todo';
 import styles from './TodoList.module.css';
 
 const TodoList = ({ filter }) => {
-  const [todos, setTodos] = useState([]);
+  /*
+   ❗️❗️ 함수를 호출해서 데이터를 읽어오거나 로컬스토리지를 사용하거나 파일을 사용하거나 네트워크상에서 읽어오는 등의 일처리를 할 때는 위 함수(TodoList)가 호출될 때마다 반복해서 계속 읽어오게 된다.
+   내부적으로 읽어온 값대신, 내부적으로 가지고 있는 값을 쓸것이므로 UI업데이트는 되지않음. 하지만! 계속 불필요한 통신을 (읽어오게)하게된다. 
+   -> 이렇게 함수를 읽거나 무거운 함수를 처리하거나 등의 일을 방지하기 위해서는 콜백으로 감싸주기
+   */
+  // const [todos, setTodos] = useState(readTodosFromLocalStorage());
+  const [todos, setTodos] = useState(() => readTodosFromLocalStorage());
 
   const handleAdd = (todo) => setTodos([...todos, todo]);
 
@@ -13,6 +19,10 @@ const TodoList = ({ filter }) => {
 
   const handleDelete = (deleted) =>
     setTodos(todos.filter((todo) => todo.id !== deleted.id));
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
 
   const filtered = getFilteredItems(todos, filter);
 
@@ -35,6 +45,11 @@ const TodoList = ({ filter }) => {
   );
 };
 
+function readTodosFromLocalStorage() {
+  console.log('hi');
+  const todos = localStorage.getItem('todos');
+  return todos ? JSON.parse(todos) : [];
+}
 // 함수내부의 다른 상태와 밀접하게 연관있는 함수가 아니라면, 차라리 인자로 받아서 외부에 선언하는것이 메모리측면에서 좋다.
 const getFilteredItems = (todos, filter) => {
   if (filter === 'all') {
