7733167d337b1595b20bc2aa181319b154968976
seul <kseul06@gmail.com>
2023-02-11 15:45:31 +0900

    Solution : DarkMode 셋팅 (useContext)
    - 다크모드 UI 적용
    - 다크모드 기억하기

--- src/Solution/Solution.jsx
+++ src/Solution/Solution.jsx
@@ -2,17 +2,18 @@ import './Solution.css';
 import TodoList from './components/TodoList/TodoList';
 import Header from './components/Header/Header';
 import { useState } from 'react';
+import { DarkModeProvider } from './context/DarkModeContext';
 
 const filters = ['all', 'active', 'completed']; // 1)고정된 필터배열이 있음
 
 const Solution = () => {
   const [filter, setFilter] = useState(filters[0]);
   return (
-    <>
+    <DarkModeProvider>
       {/* 모든 필터 정보, 현재 필터 정보, 필터변경시 호출할 setFilter */}
       <Header filters={filters} filter={filter} onFilterChange={setFilter} />
       <TodoList filter={filter} />
-    </>
+    </DarkModeProvider>
   );
 };
 
--- src/Solution/components/Header/Header.jsx
+++ src/Solution/components/Header/Header.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { useDarkMode } from '../../context/DarkModeContext';
 import styles from './Header.module.css';
+import { HiMoon, HiSun } from 'react-icons/hi';
 
 const Header = ({ filters, filter, onFilterChange }) => {
+  const { darkMode, toggleDarkMode } = useDarkMode();
+
   return (
     <header className={styles.header}>
+      <button onClick={toggleDarkMode} className={styles.toggle}>
+        {darkMode ? <HiMoon /> : <HiSun />}
+      </button>
       <ul className={styles.filters}>
         {filters.map((value, i) => (
           <li key={i}>
